20d90b34850a47c7f4d7414097d1bfe444fd4fda
Filipe <filipemonferna12@gmail.com>
2024-02-29 07:59:03 -0300

    ...

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     platform: 'github',
-    token: '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',
+    token: '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',
     logFileLevel: 'depurar',
     logLevel: 'depurar',
     repositories: ['fillipim/teste-renovate'],
