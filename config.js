15ec32c3fa3fe947a25007623e9994b24d30037a
Filipe <filipemonferna12@gmail.com>
2024-02-29 07:43:47 -0300

    ...

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    platform: 'github',
+    token: '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',
+    logFileLevel: 'depurar',
+    logLevel: 'depurar',
+    repositories: ['fillipim/teste-renovate'],
+};
